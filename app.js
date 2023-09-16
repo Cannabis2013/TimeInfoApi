09fc6d2ffe6c4c363e7a6b52cb9191efde9044d5
Cannabis2013 <MH2012@outlook.dk>
2023-09-17 00:24:19 +0200

    Fixed a little time issue. Should work now

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}/`)
 })
-
 
 app.get('/',(req,res) => {
     return res.sendFile(__dirname + "/views/index.html")
